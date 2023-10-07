e9b3c51660c7b2081c51a9ff09e821cb9f990bcd
dilqvl62 <sab38m@hotmail.com>
2023-10-06 22:02:24 -0500

    added a book components

---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+const Books  = () => {
+   
+    return (
+        <p>Add Some Books Here</p>
+    );
+}
+
+export default Books
